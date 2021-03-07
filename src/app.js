72bb5d61b0f4570f6ec1a907710c33b0b9ed06ca
Irina Stepanova <irina.s.vesna@mail.ru>
2021-03-07 19:05:23 +0600

    feat: update app.js

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const { StatusCodes, ReasonPhrases } = require('http-status-codes');
+const countryRouter = require('./resources/countries/country.router');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use('/favicon.ico', (req, res) => res.sendStatus(StatusCodes.NO_CONTENT));
+
+app.use('/countries', countryRouter);
+
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_IMPLEMENTED).send(ReasonPhrases.NOT_IMPLEMENTED);
+});
+
+module.exports = app;
