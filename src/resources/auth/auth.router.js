c8cf9a04223aab0ff637014790c94529ef5c3341
Velidoss <godofthemountain09@gmail.com>
2021-03-12 20:49:29 +0200

    refactor: refactored 1-line conditional statements

--- src/resources/auth/auth.router.js
+++ src/resources/auth/auth.router.js
@@ -12,10 +12,14 @@ router.post('/register',  async (req, res) => {
   const {name, email, password} = req.body;
   const {error} = registerValidation(req.body);
 
-  if(error) return res.status(400).send(error.details[0].message)
+  if (error) {
+    return res.status(400).send(error.details[0].message)
+  }
 
   const emailExistsInDB = await User.findOne({email: req.body.email});
-  if (emailExistsInDB) return res.status(400).send('Email already exists');
+  if (emailExistsInDB) {
+    return res.status(400).send('Email already exists');
+  }
 
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
@@ -36,11 +40,17 @@ router.post('/register',  async (req, res) => {
 
 router.post('/token', async(req, res) => {
   const {refreshToken} = req.body;
-  if (!refreshToken) return res.status(401);
+  if (!refreshToken) {
+    return res.status(401);
+  }
   const validToken = await RefreshToken.findOne({refreshToken});
-  if(!validToken) return res.sendStatus(403);
+  if(!validToken) {
+    return res.sendStatus(403);
+  }
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, user) => {
-    if(error) return res.sendStatus(403);
+    if(error) {
+      return res.sendStatus(403);
+    }
     const accessToken = jwt.sign({_id: user._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'});
     res.json({accessToken})
   })
@@ -55,13 +65,19 @@ router.post('/login', async (req, res) => {
   const {email, password} = req.body;
   const {error} = loginValidation(req.body);
 
-  if(error) return res.status(400).send(error.details[0].message);
+  if(error) {
+    return res.status(400).send(error.details[0].message);
+  }
 
   const userRegistered = await User.findOne({email});
-  if (!userRegistered) return res.status(400).send('There is no such user registered.');
+  if (!userRegistered) {
+    return res.status(400).send('There is no such user registered.');
+  }
 
   const validPassword = await bcrypt.compare(password, userRegistered.password);
-  if(!validPassword) return res.status(400).send('Wrong password');
+  if(!validPassword) {
+    return res.status(400).send('Wrong password');
+  }
 
   const token = jwt.sign({_id: userRegistered._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '15m'});
   const refreshToken = jwt.sign({_id: userRegistered._id}, process.env.REFRESH_TOKEN_SECRET);
@@ -74,8 +90,6 @@ router.post('/login', async (req, res) => {
   }catch(error){
     res.status(400).send(error)
   }
-  
-
 });
 
 module.exports = router;
