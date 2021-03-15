5b1096a4d7ad5bd47c25f2c3699735f0c9d5f784
Irina Stepanova <irina.s.vesna@mail.ru>
2021-03-15 07:27:21 +0600

    feat: add fields to country-model

--- src/resources/countries/country.model.js
+++ src/resources/countries/country.model.js
@@ -13,6 +13,10 @@ const sightsSchema = new Schema({
     type: String,
     required: true,
   },
+  image: {
+    type: String,
+    required: true,
+  },
 });
 
 const countrySchema = new Schema({
@@ -38,6 +42,30 @@ const countrySchema = new Schema({
     required: true,
   },
   sights: [sightsSchema],
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  capitalEN: {
+    type: String,
+    required: true,
+  },
+  capitalGMT: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  card: {
+    type: String,
+    required: true,
+  }
 });
 
 const Country = model('Country', countrySchema);
